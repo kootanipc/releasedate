069465dfd20c1e4735a503df15230c2ad850a9c7
kazuya ootani <k-ootani@nttpc.co.jp>
2018-03-27 16:41:46 +0000

    20180328-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,24 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
   has_many :releases, dependent: :destroy
 
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
+
+    unless user
+      user = User.new(
+          name:     auth.extra.raw_info.name,
+          provider: auth.provider,
+          uid:      auth.uid,
+          email:    "#{auth.uid}-#{auth.provider}@example.com",
+          image_url:   auth.info.image,
+          password: Devise.friendly_token[0, 20]
+      )
+      user.skip_confirmation!
+      user.save(validate: false)
+    end
+    user
+  end
+
   def self.find_for_twitter_oauth(auth, signed_in_resource = nil)
     user = User.find_by(provider: auth.provider, uid: auth.uid)
 
